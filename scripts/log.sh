bbcf2f4c00d4e2b7579ac9153224fefefa7a61bd
prb71 <96513118+prb71@users.noreply.github.com>
2023-03-23 10:52:46 +0530

    Update log.sh

--- scripts/log.sh
+++ scripts/log.sh
@@ -2,7 +2,7 @@
 
 TARGET_ID=$(aws deploy list-deployment-targets --deployment-id $DEPLOYMENT_ID  --region us-east-2  | jq .targetIds | sed 's/\"//g'| sed 's/\[//g' | sed 's/\]//g')
 
-aws deploy batch-get-deployment-targets --deployment-id $DEPLOYMENT_ID --region us-east-2  --target-ids TARGET_ID | grep -i scriptfailed 
+aws deploy batch-get-deployment-targets --deployment-id $DEPLOYMENT_ID --region us-east-2  --target-ids $TARGET_ID | grep -i scriptfailed 
 
 if [ echo $? -eq 0 ]; then
 
